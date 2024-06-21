40d697b5af4da5ea84f6a5ee502e6aeab248c739
Saleem Toure <saleemtoure@gmail.com>
2024-06-22 01:16:21 +0200

    feat: :sparkles: The program will now remember what you last had in your to-do-list (+ autosave)

--- Program/to_do_list.py
+++ Program/to_do_list.py
@@ -1,3 +1,4 @@
+from email.policy import default
 import os
 import tkinter as tk
 from tkinter import messagebox
@@ -26,24 +27,40 @@ class ListGUI:
 
         self.treeView.bind("<<TreeviewSelect>>", self.delete_objective)
 
-        self.out_btn = ttk.Button(text="Save as txt file", command=self.save)
-        self.out_btn.pack(pady=2)
-
         self.inn_btn = ttk.Button(text="Load list from txt file", command=self.load)
         self.inn_btn.pack()
 
-        self.clear_btn = ttk.Button(text="Clear", command=self.reset)
+        self.clear_btn = ttk.Button(text="Clear", command=self.clear)
         self.clear_btn.pack(padx=10, pady=10)
 
-        self.saved = False
+        # Open list from last time - START
+
+        dir_path = os.path.realpath(
+            os.path.join(os.getcwd(), os.path.dirname(__file__))
+        )
+        file_path = os.path.join(dir_path, "my-to-do-list.txt")
+
+        if not os.path.exists(file_path):
+            pass
+        else:
+            with open(file_path, "r") as f:
+                for line in f:
+                    clean_line = line.strip()
+                    if clean_line:
+                        self.treeView.insert("", "end", text=clean_line)
+
+        self.autosave()
+        # Open list from last time - END
 
         self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
         self.root.mainloop()
 
     def add_objective(self, event):
-        self.treeView.insert("", "end", text=self.entrybox.get())
-        self.entrybox.delete(0, tk.END)
-        self.saved = False
+        if self.entrybox.get() != "":
+            self.treeView.insert("", "end", text=self.entrybox.get())
+            self.entrybox.delete(0, tk.END)
+
+        self.autosave()
 
     def delete_objective(self, event):
         if self.treeView.selection() == ():
@@ -53,27 +70,20 @@ class ListGUI:
                 title="Quit?", message="Do you want to delete this task?"
             ):
                 self.treeView.delete(self.treeView.selection()[0])
-        self.saved = False
 
-    def save(self):
-        desktop_file_path = os.path.join(os.path.expanduser("~"), "Desktop")
+        self.autosave()
 
-        file_path = filedialog.asksaveasfilename(
-            initialfile="my-to-do-list",
-            initialdir=desktop_file_path,
-            defaultextension=".txt",
-            filetypes=[("Text Files", "*.txt")],
+    def autosave(self):
+        dir_path = os.path.realpath(
+            os.path.join(os.getcwd(), os.path.dirname(__file__))
         )
+        file_path = os.path.join(dir_path, "my-to-do-list.txt")
 
-        if not file_path:
-            pass
-        else:
-            # print(file_path)
-            with open(file_path, "w") as f:
-                for task in self.treeView.get_children():
-                    f.write(f"{self.treeView.item(task)['text']}\n")
-
-            self.saved = True
+        with open(file_path, "w") as f:
+            for task in self.treeView.get_children():
+                if task != "":
+                    taskText = self.treeView.item(task)["text"]
+                    f.write(f"{taskText}\n")
 
     def load(self):
         file_path = filedialog.askopenfilename(filetypes=[("Text Files", "*.txt")])
@@ -81,22 +91,19 @@ class ListGUI:
             pass
         else:
             f = open(file_path, "r")
-            self.reset()
+            self.clear()
             for line in f:
                 self.treeView.insert("", "end", text=line)
             f.close()
-        self.saved = True
 
-    def reset(self):
+    def clear(self):
         for task in self.treeView.get_children():
             self.treeView.delete(task)
-        self.saved = False
+
+        self.autosave()
 
     def on_closing(self):
         if messagebox.askyesno(title="Quit?", message="Do you really want to quit?"):
-            if len(self.treeView.get_children()) != 0 and self.saved == False:
-                self.save()
-
             self.root.destroy()
 
 
